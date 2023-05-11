a2288ca40741f24e689be80a304ef4632277dd04
KK2313 <j.i.kimingichi@gmail.com>
2023-05-11 04:46:50 -0700

    Task 0

---
+++ betty_checks.c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+
+int main()
+{
+	int n1, n2, product;
+
+	printf("Enter you first integer: ");
+	scanf("%d", &n1);
+
+	printf("Enter your second interger: ");
+	scanf("%d", &n2);
+
+	product = n1 * n2;
+
+	printf("The product of %d and %d is %d\n", n1, n2, product);
+
+	return(0);
+}
