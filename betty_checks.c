8980e8f59d1835556697c8f8d1eda5567861b06d
netia-melvin <melvinatemba18@gmail.com>
2023-05-26 10:40:50 -0700

    simple-shell

--- betty_checks.c
+++ betty_checks.c
@@ -1,27 +1,13 @@
-#include  <stdio.h>
-
-/**
- * main - Entry point of the program
- *
- * Description: This program evaluates the product
- * 		of two integers
- *
- * Return: 0 success.
- */
-
-int main(void)
-{
-	int n1, n2, product;
-
-	printf("Enter you first integer: ");
-	scanf("%d", &n1);
-
-	printf("Enter your second interger: ");
-	scanf("%d", &n2);
-
-	product = n1 * n2;
-
-	printf("The product of %d and %d is %d\n", n1, n2, product);
-
-	return (0);
+#include <unistd.h> 
+ 
+/** 
+ * _putchar - writes the character c to stdout 
+ * @c: The character to print 
+ * 
+ * Return: On success 1. 
+ * On error, -1 is returned, and errno is set appropriately. 
+ */ 
+int _putchar(char c) 
+{ 
+ return (write(1, &c, 1)); 
 }
